b17e0d929e96670e3fa115aab4f1ee6c9db2c046
henok4@gmail.com <henok4@gmail.com>
2014-04-07 14:10:00 -0400

    Basic sign in, sign out and register added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 HackerApp::Application.routes.draw do
+  devise_for :users
+  root "welcome#index"
+
+  devise_scope :user do
+    get "sign_in", to: "devise/sessions#new"
+  end
+  #match '/signin',  to: 'sessions#new',         via: 'get'
+  #match '/signout', to: 'sessions#destroy',     via: 'delete'
+  #match '/help',    to: 'static_pages#help',    via: 'get'
+  #match '/about',   to: 'static_pages#about',   via: 'get'
+  #match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
